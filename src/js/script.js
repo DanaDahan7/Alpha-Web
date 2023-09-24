43ece36546e55ef5a5502d0f3222591b4a5d23f4
DanaDahan7 <99274381+DanaDahan7@users.noreply.github.com>
2023-09-24 15:54:47 +0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,5 @@
 $("#navbar-toggle-btn").click(function () {
     $(".navbar-collapse").toggleClass("show");
-    $(".navbar-toggler-arrow").toggle(); // Toggle the arrow button
 });
 
 const observer = new IntersectionObserver((entries) => {
